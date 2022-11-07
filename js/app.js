d067f5d644551dc2cd273a4ba9bf067c11192e2f
Mauro Rivera <mauroriverae@gmail.com>
2022-11-07 20:09:22 -0300

    Solutions errors

--- js/app.js
+++ js/app.js
@@ -1,16 +1,28 @@
 "use strict";
-const API_URL = "http://localhost/TPE2F/api/company";
+const api = "http://localhost/web2/TpFinal/Tp_webdos_fina/api/company";
 
 async function getCompany(){
-    try{
-        let response = await fetch(API_URL);
-        let companies = await response.json();
-        console.log(companies);
-    } catch(e){
+    try {
+        let response = await fetch(api);
+        if (response.ok){
+            let companies = await response.json();
+            showCompany(companies)
+        }else{
+            throw new Error("Recurso no existente");
+        }
+    } catch (e) {
         console.log(e);
+    }
+
+}
+
+
+function showCompany(companies){
+    let ul = document.querySelector("#li");
+    ul.innerHTML = "";
+    for (const company of companies) {
+        ul.innerHTML += `<li>${company.Tiker}</li>`;
     }
 }
 
 getCompany();
-
-// 42min y 34s 25/10
