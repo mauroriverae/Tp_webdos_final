8a58559b8d7a169ab94080c4df9b38b2939c48d2
Mauro Rivera <mauroriverae@gmail.com>
2022-11-07 21:31:05 -0300

    Adding Add from CSR frontend

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 "use strict";
-const api = "http://localhost/web2/TpFinal/Tp_webdos_fina/api/company";
+const api = "http://localhost/web2/TpFinal/Tp_webdos_final/api/company";
+
+let form = document.querySelector('#form_add');
+form.addEventListener('submit', addCompany);
 
 async function getCompany(){
     try {
@@ -21,7 +24,42 @@ function showCompany(companies){
     let ul = document.querySelector("#li");
     ul.innerHTML = "";
     for (const company of companies) {
-        ul.innerHTML += `<li>${company.Tiker}</li>`;
+        ul.innerHTML += `<tr>
+                            <td>${company.Tiker}</td>
+                            <td>${company.Company}</td>
+                            <td>${company.Sector}</td>
+                            <td>
+						        <a type='button' class='btn btn-danger'>Borrar</a>
+						        <a data-task${company.id} type='button' class='btn btn-warning btn-delete'>modificar</a>
+					        </td>
+                         </tr>`;
+    }
+}
+
+
+async function addCompany(e){
+    e.preventDefault();
+    let data = new FormData(form);
+
+    let company = {
+        Company: data.get('Company'),
+        Sector: data.get('Sector'),
+        Tiker: data.get('Tiker'),
+    };
+
+    try {
+        let response = await fetch(api, {
+            method: "POST",
+            headers: {'Content-Type' : 'application/json'},
+            body: JSON.stringify(company)
+        });
+        if (!response.ok)
+            throw new Error('Recurso no existe');
+        getCompany();
+        form.reset()
+
+    } catch (e) {
+        console.log(e)
     }
 }
 
